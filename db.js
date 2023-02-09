ad55ebcac50f6ecf91188f0b4894260b23296f88
shubhambargal2002 <shubhambargal2002@gmail.com>
2023-02-09 12:18:46 +0530

    db.js changes

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 const mongoose=require('mongoose');
 const DATABASE = process.env.DATABASE;
 
+mongoose.set('strictQuery', false);
+
 mongoose.connect(DATABASE).then(()=>{
     console.log("connected to mongoDB successfully")
 }).catch((err)=>console.log("error"))
